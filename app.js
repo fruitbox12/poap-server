c0b2a6adf8bdcb1a3a4935591fa34a4ba4f7f7b5
iamminci <0xminci@gmail.com>
2022-08-10 15:00:47 -0400

    ADDED: dynamic deploy route

--- app.js
+++ app.js
@@ -1,13 +1,29 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
+
+// middleware
+app.use(express.json());
+app.use(express.urlencoded());
+
 const port = process.env.PORT || 3001;
 
 const { deploy } = require("./scripts/deploy");
 
+app.use(cors());
+
 app.get("/", async (req, res) => {
-  const deployedContract = await deploy(100);
-  res.send(deployedContract.address);
-  //   res.send("Hi there");
+  res.send("Hi there");
+});
+
+app.post("/deploy", async (req, res) => {
+  console.log("req: ", req.body.tokenSupply);
+
+  const tokenSupply = req.body.tokenSupply;
+  const deployedContract = await deploy(tokenSupply);
+  res.send({
+    message: `Contract successfully deployed at: ${deployedContract.address}`,
+  });
 });
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));

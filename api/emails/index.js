938d1f4c0f2af6eb3b80edba835872b5ccbf3821
fruitbox12 <dylanwong007@gmail.com>
2023-05-16 12:28:10 -0800

    Update index.js

--- api/emails/index.js
+++ api/emails/index.js
@@ -1,9 +1,9 @@
 module.exports = (req, res) => {
   // Mocked data
   const messages = [
-    { id: '1', subject: 'First email', sender: 'sender@example.com', body: 'Hello!' },
-    { id: '2', subject: 'Second email', sender: 'sender@example.com', body: 'Hi there!' },
-    { id: '3', subject: 'Third email', sender: 'sender@example.com', body: 'How are you?' }
+    { id: '1', subject: 'First email', sender: 'dylanwong007@gmail.com', body: 'Hello check out chatgpt!' },
+    { id: '2', subject: 'Second email', sender: 'dylanwong007@gmail.com', body: 'Hi there I am Dylan ncie to meet!' },
+    { id: '3', subject: 'Third email', sender: 'dylanwong007@gmail.com', body: 'How are you doing, i know its been tough with the finals?' }
   ];
 
   const email = 'sgrady@innerexplorer.org';
